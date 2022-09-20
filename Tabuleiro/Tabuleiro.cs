5d77dba726c4c6ab7d387ac32a38ec7af31dba29
Davi <davi_martins2009@outlook.com>
2022-09-20 11:16:53 -0300

    Implementando Tabuleiro

--- Tabuleiro/Tabuleiro.cs
+++ Tabuleiro/Tabuleiro.cs
@@ -1,19 +1,16 @@
 ﻿namespace tabuleiro
 {
-    internal class Posicao
+    internal class Tabuleiro
     {
         public int linha { get; set; }
         public int coluna { get; set; }
+        private Peca[,] pecas;
 
-        public Posicao(int linha, int coluna)
+        public Tabuleiro(int linha, int coluna)
         {
             this.linha = linha;
             this.coluna = coluna;
-        }
-
-        public override string ToString()
-        {
-            return linha + ", " + coluna;
+            pecas = new Peca[linha, coluna];
         }
     }
 }
